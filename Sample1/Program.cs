00862e33612f9f40d2bdfbeb90b64152ffe550b5
Michael <Michael@home.de>
2017-11-17 20:42:14 +0100

    Projektdateien hinzufügen.

---
+++ Sample1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//using Moq;
+
+namespace Sample1
+{
+    class Foo
+    {
+        public void Bar(Action<int> action)
+        {
+            action.Invoke(4);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+
+            var foo = new Foo();
+
+            foo.Bar( Test );
+            foo.Bar( i => { Console.WriteLine(i); });
+
+            Console.ReadKey();
+        }
+
+        static void Test(int i)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
---
+++ Sample2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample2
+{
+    public class Bar
+    {
+        ~Bar()
+        {
+            Console.WriteLine("dtor ~Bar()");
+        }
+    }
+
+    public class Foo
+    {
+        private Bar _bar;
+        public Bar Bar
+        {
+            get { return this._bar; }
+        }
+
+
+        public Foo(Bar bar)
+        {
+            this._bar = bar;
+        }
+
+        ~Foo()
+        {
+            Console.WriteLine("dtor ~Foo()");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bar = new Bar();
+            var foo = new Foo(bar);
+
+            var bar2 = foo.Bar;
+
+            bar = null;
+            foo = null;
+            GC.Collect();
+
+            Console.ReadKey();
+
+        }
+    }
+}
