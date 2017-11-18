8a52c62b0d89e67b5a6dc7aca6d79559e27b5022
Michael <Michael@home.de>
2017-11-18 22:18:27 +0100

    Interlocked

---
+++ Sample3/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Sample3
+{
+    class Program
+    {
+        static Int64 Counter;
+
+        static void Main(string[] args)
+        {
+            var t = new Thread(Go);
+            t.Start();
+
+            while(true)
+            {
+                //var copy = Counter;
+                var copy = Interlocked.Read(ref Counter);
+
+                if(copy != 0 &&
+                   copy != Int64.MaxValue)
+                {
+                    Console.WriteLine("{0:X}", copy);
+                }
+            }
+        
+            Console.ReadKey();
+        }
+
+        private static void Go()
+        {
+            while(true)
+            {
+                //Counter = 0;
+                //Counter = Int64.MaxValue;
+
+                var replaced = Interlocked.CompareExchange(ref Counter, 0, Int64.MaxValue);
+
+                replaced = Interlocked.CompareExchange(ref Counter, Int64.MaxValue, 0);
+
+            }
+        }
+    }
+}
